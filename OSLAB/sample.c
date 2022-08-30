8d29d30073c1aade053e6b6b0d192b8d4e0c0e0a
cganesh-kumar-reddy <94513366+cganesh-kumar-reddy@users.noreply.github.com>
2022-08-30 11:04:32 +0530

    Create sample.c

---
+++ OSLAB/sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: c.ganesh kumar reddy
+   Roll No: 204G1A0531
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
